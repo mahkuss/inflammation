52230616ceba36ad0a27ec79e86bb66ddea1e9f2
mahkus <anonymouse@xqua.org>
2014-11-08 15:50:08 -0500

    Modified add_data.sh to create directories as needed.

--- bin/add_data.sh
+++ bin/add_data.sh
@@ -1,7 +1,8 @@
 DRUG=$(python /home/rob/Desktop/inflammation/src/assign_drug.py $2)
 DEST=data/$1/$1-$DRUG.dat
+mkdir -p data/$1
 cp $2 $DEST
 git add $DEST
 MESSAGE="Add file $DEST to data."
-git commit -m "'$MESSAGE'"
+git commit -m \"$MESSAGE\"
 echo "New file added to th repo: $DEST"
