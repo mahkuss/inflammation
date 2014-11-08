3c420f1081b31a33c388d56b3c5c16ee89dafde7
mahkus <anonymouse@xqua.org>
2014-11-08 15:46:21 -0500

    Change add_data.sh to automatically add and commit data to repo.

--- bin/add_data.sh
+++ bin/add_data.sh
@@ -1,3 +1,7 @@
 DRUG=$(python /home/rob/Desktop/inflammation/src/assign_drug.py $2)
 DEST=data/$1/$1-$DRUG.dat
-echo cp $2 $DEST
+cp $2 $DEST
+git add $DEST
+MESSAGE="Add file $DEST to data."
+git commit -m "'$MESSAGE'"
+echo "New file added to th repo: $DEST"
